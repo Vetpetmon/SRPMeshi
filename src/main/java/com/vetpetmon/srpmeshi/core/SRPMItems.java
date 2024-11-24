0b6a76d636426173e6f015b473995f0ea250c29b
Modrome <vetpetmon@hotmail.com>
2024-11-24 13:33:30 -0500

    Update SRPMItems.java

--- src/main/java/com/vetpetmon/srpmeshi/core/SRPMItems.java
+++ src/main/java/com/vetpetmon/srpmeshi/core/SRPMItems.java
@@ -30,10 +30,13 @@ public class SRPMItems {
         tendon_sandwich = new ItemSRPFood("tendon_sandwich",5,0.25f,false).setPotionEffect(new PotionEffect(MobEffects.ABSORPTION, 1200, 0), 1.0F),
         tendon_sandwich_b = new ItemSRPFood("tendon_sandwich_b",6,0.3f,false).setPotionEffect(new PotionEffect(MobEffects.ABSORPTION, 1200, 1), 1.0F),
         sum_jelly_sandwich = new ItemSRPFood("sum_jelly_sandwich",5,0.25f,false).setPotionEffect(new PotionEffect(MobEffects.STRENGTH, 900, 1), 1.0F),
-        sum_jelly_sandwich_b = new ItemSRPFood("sum_jelly_sandwich_b",6,0.3f,false).setPotionEffect(new PotionEffect(MobEffects.STRENGTH, 900, 2), 1.0F),
+        sum_jelly_sandwich_b = new ItemSRPFood("sum_jelly_sandwich_b",6,0.3f,false).setPotionEffect(new PotionEffect(MobEffects.STRENGTH, 600, 2), 1.0F),
         core_kebab = new ItemSRPFood("core_kebab",8,0.4f,false).setPotionEffect(new PotionEffect(MobEffects.INVISIBILITY, 1200, 0), 1.0F),
         friedbeckonskins = new ItemSRPFood("friedbeckonskins",2,0.25f,false).setPotionEffect(new PotionEffect(MobEffects.HASTE, 300, 0), 1.0F),
-        rotdog = new ItemSRPFood("rotdog",6,0.3f,false).setPotionEffect(new PotionEffect(MobEffects.STRENGTH, 200, 3), 1.0F)
+        rotdog = new ItemSRPFood("rotdog",6,0.3f,false).setPotionEffect(new PotionEffect(MobEffects.STRENGTH, 200, 3), 1.0F),
+        vilebeefr = new ItemSRPFood("vilebeefr",2,0.1f,false).setPotionEffect(new PotionEffect(MobEffects.POISON, 100, 2), 1.0F),
+        vilebeefu = new ItemSRPFood("vilebeefu",4,0.15f,false).setPotionEffect(new PotionEffect(SRPPotions.COTH_E, 200, 0), 1.0F),
+        vilebeefc = new ItemSRPFood("vilebeefc",6,0.25f,false).setPotionEffect(new PotionEffect(MobEffects.STRENGTH, 400, 2), 1.0F)
 
         ;
 }
