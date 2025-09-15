96cb93f52bdc98495704ee35f10cefbf3950e542
Modrome <vetpetmon@hotmail.com>
2025-09-14 22:15:18 -0400

    TFC heat recipes + readme table

--- README.md
+++ README.md
@@ -1,6 +1,65 @@
 # Scape and Meshi
 Dungeon Meshi, but for SRP
 
+## Mod Integrations & Interactions
+<table class="tg"><thead>
+  <tr>
+    <th class="tg-0pky">Mod</th>
+    <th class="tg-0pky">Feature</th>
+    <th class="tg-0pky">Status</th>
+  </tr></thead>
+<tbody>
+  <tr>
+    <td class="tg-0pky" rowspan="2">Terrafirmacraft/Terrafirmagreg</td>
+    <td class="tg-0pky">Nutrition stats</td>
+    <td class="tg-0pky">Done, in balancing stage</td>
+  </tr>
+  <tr>
+    <td class="tg-0pky">Firepit/Charcoal Forge recipes</td>
+    <td class="tg-0pky">Done</td>
+  </tr>
+  <tr>
+    <td class="tg-0pky" rowspan="2">Scape and Run: Parasites</td>
+    <td class="tg-0pky">Loot drops (native)</td>
+    <td class="tg-0pky">SRP Patreon Build Only</td>
+  </tr>
+  <tr>
+    <td class="tg-0pky">Loot drops (1.9 support)</td>
+    <td class="tg-0pky">Not started, may be covered by SRPMixins</td>
+  </tr>
+  <tr>
+    <td class="tg-0pky" rowspan="2">Scape and Run: Parasites Extra</td>
+    <td class="tg-0pky">Food Items</td>
+    <td class="tg-0pky">Planned</td>
+  </tr>
+  <tr>
+    <td class="tg-0pky">Loot drops (native)</td>
+    <td class="tg-0pky">Planned</td>
+  </tr>
+  <tr>
+    <td class="tg-0pky" rowspan="2">Deep Sea Danger</td>
+    <td class="tg-0pky">Food Items</td>
+    <td class="tg-0pky">Planned</td>
+  </tr>
+  <tr>
+    <td class="tg-0pky">Loot drops (native)</td>
+    <td class="tg-0pky">Planned</td>
+  </tr>
+  <tr>
+    <td class="tg-0lax">Scape and Run Holiday</td>
+    <td class="tg-0lax">Food Items</td>
+    <td class="tg-0lax">Planned</td>
+  </tr>
+  <tr>
+    <td class="tg-0lax">How Do I Prove It?</td>
+    <td class="tg-0lax">No</td>
+    <td class="tg-0lax">Never</td>
+  </tr>
+</tbody></table>
+
+
+
+
 # Changelog:
 
 ## 1.7
@@ -10,6 +69,7 @@ A large amount of balancing changes based on user feedback and Parafirmagreg SMP
 - Raw meats rot faster (Terrafirmacraft integration)
 - Living armor set now craftable
 - Effect duration can now be multiplied via config option
+- Added Firepit (Heat) recipes for items (Terrafirmacraft integration)
 
 ## 1.6
 I regret to inform you that beckons are vegetarian-friendly.
--- src/main/java/com/vetpetmon/srpmeshi/Core.java
+++ src/main/java/com/vetpetmon/srpmeshi/Core.java
@@ -8,8 +8,12 @@ import com.vetpetmon.srpmeshi.core.tfc.MeshiFoodData;
 import net.dries007.tfc.api.capability.food.CapabilityFood;
 import net.dries007.tfc.api.capability.food.FoodData;
 import net.dries007.tfc.api.capability.food.FoodHandler;
+import net.dries007.tfc.api.recipes.heat.HeatRecipe;
+import net.dries007.tfc.api.recipes.heat.HeatRecipeSimple;
+import net.dries007.tfc.api.registries.TFCRegistries;
 import net.dries007.tfc.objects.inventory.ingredient.IIngredient;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.event.ModelRegistryEvent;
@@ -28,6 +32,7 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
+import net.minecraftforge.registries.IForgeRegistry;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -39,7 +44,7 @@ public class Core {
             MOD_ID = "srpmeshi",
             MOD_NAME = "SRP Meshi",
             VERSION = "1.7",
-            DEPENDENCIES = "required-after:forge@[14.21.1.2387,);required-after:srparasites@[1.9.20,)",
+            DEPENDENCIES = "required-after:forge@[14.21.1.2387,);required-after:srparasites@[1.9.20,);after:tfc",
             PROXY_COMMON = "com.vetpetmon.srpmeshi.CommonProxy",
             PROXY_CLIENT = "com.vetpetmon.srpmeshi.ClientProxy";
 
@@ -58,6 +63,7 @@ public class Core {
         }
 
         MinecraftForge.EVENT_BUS.register(this);
+
     }
 
     @Mod.EventHandler
@@ -97,6 +103,15 @@ public class Core {
         if (Loader.isModLoaded("tfc") && SRPMeshiConfig.tfcModule) {
             logger.info("We will now attempt to natively support TFC's food system.");
             attachFoodData();
+            TFCRegistries.HEAT.registerAll(
+                    new HeatRecipeSimple(IIngredient.of(SRPMItems.vilebeefr),               new ItemStack(SRPMItems.vilebeefu), 200, 480).setRegistryName("undercooked_vilebeef"),
+                    new HeatRecipeSimple(IIngredient.of(SRPMItems.vilebeefu),               new ItemStack(SRPMItems.vilebeefc), 200, 480).setRegistryName("cooked_vilebeef"),
+                    new HeatRecipeSimple(IIngredient.of(SRPMItems.raw_buglin),              new ItemStack(SRPMItems.roasted_buglin), 200, 480).setRegistryName("roasted_buglin"),
+                    new HeatRecipeSimple(IIngredient.of(SRPMItems.raw_fangs),               new ItemStack(SRPMItems.roasted_fangs), 200, 480).setRegistryName("roasted_fangs"),
+                    new HeatRecipeSimple(IIngredient.of(SRPMItems.raw_ground_tendons),      new ItemStack(SRPMItems.cooked_ground_tendons), 200, 480).setRegistryName("cooked_ground_tendons"),
+                    new HeatRecipeSimple(IIngredient.of(SRPMItems.devourercala),            new ItemStack(SRPMItems.devourercalacooked), 200, 480).setRegistryName("cooked_devourer_calamari"),
+                    new HeatRecipeSimple(IIngredient.of(SRPMItems.raw_scallops),            new ItemStack(SRPMItems.cooked_scallops), 200, 480).setRegistryName("cooked_scallops")
+            );
         }
     }
 
